3dc686ec7e0b9cadc27c2e26acceab29b926548f
Jay <sanjaysheombar@gmail.com>
2022-09-24 16:20:46 +0200

    Can detect invalid email

--- app/src/test/java/com/jaySH/friends/signup/CredentialsValidationTest.kt
+++ app/src/test/java/com/jaySH/friends/signup/CredentialsValidationTest.kt
@@ -3,14 +3,24 @@ package com.jaySH.friends.signup
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class CredentialsValidationTest {
 
-    @Test
-    fun invalidEmail() {
+    @ParameterizedTest
+    @CsvSource(
+        "'email'",
+        "'a@b.c'",
+        "'ab@b.c'",
+        "'ab@bc.c'",
+        "''",
+        "'   '",
+    )
+    fun invalidEmail(email: String) {
         val viewModel = SignUpViewModel()
 
-        viewModel.createAccount("", ":password:", ":about:")
+        viewModel.createAccount(email, ":password:", ":about:")
 
         assertEquals(SignUpState.BadEmail, viewModel.state.value.signUpState)
     }
