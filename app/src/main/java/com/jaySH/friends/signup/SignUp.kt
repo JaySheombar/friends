83ed601e002920f946ac0630084b9862f1bb44bd
Jay <sanjaysheombar@gmail.com>
2022-09-24 15:09:27 +0200

    Added UI fields

--- app/src/main/java/com/jaySH/friends/signup/SignUp.kt
+++ app/src/main/java/com/jaySH/friends/signup/SignUp.kt
@@ -1,17 +1,20 @@
 package com.jaySH.friends.signup
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jaySH.friends.R
 
 @Preview(device = Devices.PIXEL_4)
@@ -21,27 +24,86 @@ fun SignUp() {
     var password by remember { mutableStateOf("") }
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        Text(text = "Create an account")
+        ScreenTitle(title = R.string.create_account)
 
-        OutlinedTextField(
-            value = email,
-            label = { Text(text = stringResource(id = R.string.email)) },
-            onValueChange = { email = it },
-        )
+        EmailField(email = email, onValueChange = { email = it })
 
-        OutlinedTextField(
-            value = password,
-            label = { Text(text = stringResource(id = R.string.password)) },
-            onValueChange = { password = it },
-        )
+        PasswordField(password = password, onValueChange = { password = it })
 
-        Button(
-            onClick = {},
-            content = { Text(text = stringResource(id = R.string.sign_up)) },
-        )
+        SignUpButton(onClick = {})
     }
 }
+
+@Composable
+private fun SignUpButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) = Button(
+    modifier = modifier
+        .fillMaxWidth()
+        .testTag(stringResource(id = R.string.sign_up)),
+    onClick = onClick,
+    content = { Text(text = stringResource(id = R.string.sign_up)) },
+)
+
+@Composable
+private fun ScreenTitle(
+    modifier: Modifier = Modifier,
+    @StringRes title: Int,
+) = Text(
+    modifier = modifier.fillMaxWidth(),
+    text = stringResource(id = title),
+    style = typography.h4,
+)
+
+@Composable
+private fun EmailField(
+    modifier: Modifier = Modifier,
+    email: String,
+    onValueChange: (String) -> Unit,
+) = OutlinedTextField(
+    modifier = modifier
+        .fillMaxWidth()
+        .testTag(stringResource(id = R.string.email)),
+    value = email,
+    label = { Text(text = stringResource(id = R.string.email)) },
+    onValueChange = onValueChange,
+)
+
+@Composable
+private fun PasswordField(
+    modifier: Modifier = Modifier,
+    password: String,
+    onValueChange: (String) -> Unit,
+) {
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        modifier = modifier
+            .fillMaxWidth()
+            .testTag(stringResource(id = R.string.password)),
+        value = password,
+        onValueChange = onValueChange,
+        label = { Text(text = stringResource(id = R.string.password)) },
+        trailingIcon = {
+            IconButton(
+                content = { VisibilityIcon() },
+                onClick = { passwordVisible = !passwordVisible },
+            )
+        },
+        visualTransformation = if (passwordVisible) VisualTransformation.None
+        else PasswordVisualTransformation(),
+    )
+}
+
+@Composable
+private fun VisibilityIcon() = Icon(
+    painter = painterResource(id = R.drawable.ic_visibility),
+    contentDescription = stringResource(id = R.string.toggle_visibility),
+)
